5af4234450105b12c9ba2822cde73d63633af61c
Prabhashperera <prabashmperera01@gmail.com>
2026-03-17 14:16:02 +0530

    Initial commit v3

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import { connect } from 'mongoose';
+import { config } from 'dotenv';
+config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// MongoDB Connection
+const connectDB = async () => {
+  try {
+    const db = await connect(process.env.DB_URL);
+    console.log("DB Connected : " + db.connection.name);
+  } catch (error) {
+    console.error("MongoDB Connection Failed:");
+    console.error(error);
+  }
+};
+
+connectDB();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('BandVault Backend Running 🎸');
+});
 
-const connectDB = async (DB_URL) => {
-    try {
-        let db = await mongoose.connect(DB_URL) 
-        console.log("DB Connected : " + db.connection.name);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-connectDB(process.env.DB_URL);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

5dcd15fbdc89e8edf44a5a36a0eb5a37815f3c46
Prabhashperera <prabashmperera01@gmail.com>
2026-03-26 23:05:22 +0530

    update song and recordings enpoints v2

--- src/routes/SongRoute.js
+++ src/routes/SongRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addRecording, deleteSong, getAllSongs, getRecordings, saveSong } from "../controller/SongController.js";
+import { addRecording, deleteSong, editRecording, editSong, getAllSongs, getRecordings, saveSong } from "../controller/SongController.js";
 import { upload } from "../config/Multer.js";
 
 
@@ -10,5 +10,7 @@ SongRoute.post('/save-record/:songId/recordings', upload.single("audio"),  addRe
 SongRoute.get('/get-songs', getAllSongs)
 SongRoute.get('/get-recordings/:songId/recordings', getRecordings)
 SongRoute.delete('/delete-song/:id', deleteSong);
+SongRoute.put('/edit-song/:id', upload.single("image"), editSong);
+SongRoute.put('/edit-record/:songId/recordings/:recordingId', upload.single("audio"), editRecording);
 
 export default SongRoute;
